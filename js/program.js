d7664258c94a12ba9188e403e1009fb7b3df4ec4
Felipe Godinho Dal Molin <felipegodinhodalmolin@gmail.com>
2021-09-15 18:04:05 -0300

    Versão com número de tentativas

--- js/program.js
+++ js/program.js
@@ -1,27 +1,34 @@
 var numeroSecreto = parseInt(Math.random() * 11);
-
+var index = 1;
+const limite = 5;
 function Chutar() {
-    // tipo Submit
-    var chute = parseInt(document.getElementById("valor").value);
-    console.log(chute);
-
     var mensagem;
-    if (chute == numeroSecreto) {
-        mensagem = "Acertou";
-    }
-    else if (chute > 10 || chute < 0) {
-        mensagem = "Você deve digitar um número de 0 a 10";
-    }
-    else {
-        mensagem = "Errou, ";
-        if (chute > numeroSecreto) {
-            mensagem += "um pouco menos";
+    var h2Element = document.getElementById("resultado");
+    if(index <= limite) {
+        var chute = parseInt(document.getElementById("valor").value);
+        console.log(chute);
+
+        mensagem = `Tentativa ${index} de ${limite}<br>`;
+        if (chute == numeroSecreto) {
+            mensagem += "Acertou";
+            index++;
+        }
+        else if (chute > 10 || chute < 0) {
+            mensagem += "Você deve digitar um número de 0 a 10";
         }
         else {
-            mensagem += "um pouco mais";
+            mensagem += "Errou, ";
+            if (chute > numeroSecreto) {
+                mensagem += "um pouco menos";
+            }
+            else {
+                mensagem += "um pouco mais";
+            }
+            index++;
         }
     }
-    console.log(mensagem);
-    var h2Element = document.getElementById("resultado");
+    else {
+        mensagem = "Fim de jogo! Recarregue a página";
+    }
     h2Element.innerHTML = mensagem;
 }
